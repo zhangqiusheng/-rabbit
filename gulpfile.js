6b90f2ef9bc0833dac94c56c8b64db5e956409a3
zhangqiusheng <zhangqiusheng@gridsum.com>
2016-04-25 10:41:24 +0800

    重构clean任务
    把回调函数统一放入gulpTasks.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp = require('gulp'),
     del = require('del'),
     argv = require('yargs').argv,
     path = require('path'),
-    setting = require('./settings.js'),
-    GulpTask = require('./buildSrc/GulpTasks');
-var ModuleGenerator = require('./buildSrc/ModuleGenerator.js');
-var TaskExecutor = require('./buildSrc/TaskExceutor.js');
+    setting = require('./src/settings.js'),
+    GulpTask = require('./buildSrc/src/GulpTasks');
+var ModuleGenerator = require('./buildSrc/src/ModuleGenerator.js');
+var TaskExecutor = require('./buildSrc/src/TaskExceutor.js');
 var srcModules = setting.modules;
 var generator = new ModuleGenerator(srcModules);
 
@@ -15,7 +15,7 @@ gulp.task('help', function () {
     console.log('	gulp 	help			参数说明');
     console.log('	gulp 	-p			生产环境(默认)');
     console.log('	gulp 	-d 			开发环境');
-    console.log('	gulp 	-m <module>		模块打包(默认:all)')
+    //console.log('	gulp 	-m <module>		模块打包(默认:all)')
 });
 
 gulp.task('default', ['clean'], function () {
@@ -29,16 +29,9 @@ gulp.task('clean', function () {
         evr: argv.p || !argv.d
     };
     var modules = generator.getAllModules();
-    var gClean = function (module, isDevelop) {
-
-        var buildType = isDevelop ? module.production : module.develop;
-        var cleanPath = path.join(module.name, buildType);
-        del(cleanPath);
-
-    };
-    // var gulpTask = new GulpTask();
-    // var gClean = gulpTask.clean(module, option.evr);
-    var taskExecutor = new TaskExecutor(modules, option.evr, gClean);
+    var gulpTask = new GulpTask();
+    var gClean = gulpTask.clean;
+    var taskExecutor = new TaskExecutor(modules, option.evr, gClean(module, option.evr));
     taskExecutor.execute();
 
 });
